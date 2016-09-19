8b8d8e184028cda8e8cc72b36df5e9baaaa82ffb
Bill Zito <bill.r.zito@gmail.com>
2016-09-19 10:47:09 -0700

    Grub and bee finished

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,9 @@
 var HoneyMakerBee = function() {
+	Bee.call(this);
+
 };
 
+//sets prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+//sets constructor
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
